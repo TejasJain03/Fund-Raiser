4168eb3c8be4da8ab44f6b7d9825d22cd2973b6d
unknown <tejaskjain2003@gmail.com>
2024-03-16 20:05:00 +0530

    Fund Raiser

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL:"http://localhost:8080/api",
-  baseURL:"https://fund-raiser-server.vercel.app",
+  baseURL:"https://fund-raiser-server.vercel.app/api",
   withCredentials: true,
 });
 
